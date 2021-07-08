5a8e480ae6dd93f009953a660afb87b301fccca0
Mihaela Shikova <82180070+Desdemonna@users.noreply.github.com>
2021-07-08 18:58:49 +0300

    Add files via upload

---
+++ C# OOP Inheritance/Person/StartUp.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Person
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            string name = Console.ReadLine();
+            int age = int.Parse(Console.ReadLine());
+
+            Person person;
+            if (age <= 15)
+            {
+                person = new Child(name, age);
+            }
+            else
+            {
+                person = new Person(name, age);
+            }
+
+            Console.WriteLine(person);
+        }
+    }
+}
---
+++ C# OOP Inheritance/Restaurant/Cake.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Restaurant
+{
+    public class Cake : Dessert
+    {
+        private const double DefaultCakeGrams = 250;
+        private const double DefaultCakeCalories = 1000;
+        private const decimal DefaultCakePrice = 5m;
+        public Cake(string name) : base(name, DefaultCakePrice, DefaultCakeGrams, DefaultCakeCalories)
+        {
+        }
+    }
+}
