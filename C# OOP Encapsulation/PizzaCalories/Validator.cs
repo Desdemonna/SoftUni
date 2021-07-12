f8e0310904b13806d7bf5cb371fa48e6651cf9b1
Mihaela Shikova <82180070+Desdemonna@users.noreply.github.com>
2021-07-12 20:50:48 +0300

    Add files via upload

---
+++ C# OOP Encapsulation/PizzaCalories/Validator.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PizzaCalories
+{
+    public static class Validator
+    {
+        public static void ThrowIfNumberIsNotInRange(int min, int max, int number, string exceptionMessage)
+        {
+            if (number < min || number > max)
+            {
+                throw new ArgumentException("");
+            }
+        }
+    }
+}
