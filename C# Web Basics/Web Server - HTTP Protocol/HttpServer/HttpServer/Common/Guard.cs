7ad7808e945a83371c5b1379adc7432a76ccf703
Mihaela Shikova <82180070+Desdemonna@users.noreply.github.com>
2022-02-12 13:20:32 +0200

    Add files via upload

---
+++ C# Web Basics/Web Server - HTTP Protocol/HttpServer/HttpServer/Common/Guard.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace HttpServer.Common
+{
+    public static class Guard
+    {
+        public static void AgainstNull(object value, string name = null)
+        {
+            if (value == null)
+            {
+                name ??= "Value";
+
+                throw new ArgumentException($"{name} can not be null");
+            }
+        }
+    }
+}
